6227df957ff257aa4d7e153c4a337f0a3cd06146
raviCodez <ravivarmanb025@gmail.com>
2024-10-06 23:00:44 +0530

    Time: 2 ms (61.69%), Space: 8.3 MB (48.42%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        int carry=0;
+        int i=a.size()-1;
+        int j=b.size()-1;
+        while (i>=0 ||j>=0||carry){
+            if(i>=0) carry +=a[i--] - '0';
+            if(j>=0) carry +=b[j--] - '0';
+            
+            result += (carry%2)+'0';
+            carry/=2;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+        
+    }
+};
